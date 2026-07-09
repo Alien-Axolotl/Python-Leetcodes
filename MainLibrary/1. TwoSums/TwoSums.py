24faea970d8325d393933fda630457c939d6f54a
AlienAxolotl <efegokturk719@gmail.com>
2026-07-09 22:10:51 +0300

    Fix syntax errors in TwoSums.py

--- MainLibrary/1. TwoSums/TwoSums.py
+++ MainLibrary/1. TwoSums/TwoSums.py
@@ -6,11 +6,11 @@ class Solution:
         n = len(nums)
         
         for i in range(n):
-            hashmap[nums[i] = i:
+            hashmap[nums[i]] = i
        
         for i in range(n):
-        difference = target - nums[i]
-        if difference in hashmap and hashmap[difference] != i:
-        return [i, hashmap[difference]]
+            difference = target - nums[i]
+            if difference in hashmap and hashmap[difference] != i:
+                return [i, hashmap[difference]]
 
-    return [] # If no solution is found , return an empty list
+        return [] # If no solution is found , return an empty list

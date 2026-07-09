d920dd4d0eaeb2ede65e2523637db928041d07a9
AlienAxolotl <efegokturk719@gmail.com>
2026-07-09 22:02:21 +0300

    Create TwoSums.py

---
+++ MainLibrary/1. TwoSums/TwoSums.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashmap = {}
+
+        n = len(nums)
+        
+        for i in range(n):
+            hashmap[nums[i] = i:
+       
+        for i in range(n):
+        difference = target - nums[i]
+        if difference in hashmap and hashmap[difference] != i:
+        return [i, hashmap[difference]]
+
+    return [] # If no solution is found , return an empty list
